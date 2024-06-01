a5b4cea9074823eed3535ac1c85f262ee56261e9
Utyfua <utyfua@gmail.com>
2024-06-01 16:58:05 +0200

    feat: fix repository generic

--- src/repository/Repository.ts
+++ src/repository/Repository.ts
@@ -4,34 +4,34 @@ import { convertWhere } from "../mappers/where"
 import { FindOneOptions, FindOptions, FindResult } from "../types"
 import { isEmptyObject } from "../utils";
 
-export function Repository<Entity extends new (...args: any[]) => any>(entity: Entity) {
+export function Repository<Entity extends object>(entity: new (...args: any[]) => Entity) {
     return class Repository extends entity {
-        static async get<T extends object>(this: { new(): T }, id: string) {
+        static async get<T extends Entity>(this: { new(): T }, id: string) {
             const { connection } = internalStorage.getEntityDefinition(this)
             const result = await connection.hGetAll(id)
             if (!isEmptyObject(result)) return undefined;
             return { id, value: result } as FindResult<T>
         }
 
-        static async set<T extends object>(this: { new(): T }, id: string, value: Partial<T>) {
+        static async set<T extends Entity>(this: { new(): T }, id: string, value: Partial<T>) {
             const { connection } = internalStorage.getEntityDefinition(this)
             const result = await connection.hSet(id, Object.entries(JSON.parse(JSON.stringify(value))))
             if (!result) return undefined;
             return { id, value } as FindResult<T>
         }
 
-        static async create<T extends object>(this: { new(): T }, value: T, id?: string) {
+        static async create<T extends Entity>(this: { new(): T }, value: T, id?: string) {
             const { schema } = internalStorage.getEntityDefinition(this)
             const hKey = id || `${schema.name}:${uuidv4()}`
             return await Repository.set.call(this, hKey, value) as FindResult<T>
         }
 
-        static async delete<T extends object>(this: { new(): T }, id: string) {
+        static async delete<T extends Entity>(this: { new(): T }, id: string) {
             const { connection } = internalStorage.getEntityDefinition(this)
             return await connection.del(id)
         }
 
-        static async find<T extends object>(this: { new(): T }, { where, options }: FindOptions<T>) {
+        static async find<T extends Entity>(this: { new(): T }, { where, options }: FindOptions<T>) {
             const { connection, schema } = internalStorage.getEntityDefinition(this)
             const query = convertWhere(
                 schema,
@@ -44,7 +44,7 @@ export function Repository<Entity extends new (...args: any[]) => any>(entity: E
             return documents as FindResult<T>[]
         }
 
-        static async findOne<T extends object>(this: { new(): T }, { where, options }: FindOneOptions<T>) {
+        static async findOne<T extends Entity>(this: { new(): T }, { where, options }: FindOneOptions<T>) {
             const [result] = await Repository.find.call(this, {
                 where, options: { ...options, limit: { from: options?.limit.from || 0, size: 1 } }
             })
